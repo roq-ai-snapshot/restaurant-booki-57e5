6b516e4da4c3e0c3ddee1905ab3a99cbc05f16e9
roq-ai-snapshot <fabian.wesner@roq.tech>
2023-08-31 04:45:12 +0000

    Initial commit

---
+++ src/config/app.config.ts
@@ -0,0 +1,20 @@
+interface AppConfigInterface {
+  ownerRoles: string[];
+  customerRoles: string[];
+  tenantRoles: string[];
+  tenantName: string;
+  applicationName: string;
+  addOns: string[];
+  ownerAbilities: string[];
+  customerAbilities: string[];
+}
+export const appConfig: AppConfigInterface = {
+  ownerRoles: ['Owner'],
+  customerRoles: ['Guest'],
+  tenantRoles: ['Owner', 'Chef', 'Waiter'],
+  tenantName: 'Restaurant',
+  applicationName: 'Restaurant booking engine',
+  addOns: ['file upload', 'chat', 'notifications', 'file'],
+  customerAbilities: ['View restaurant profiles', 'View restaurant menus', 'Manage table reservations'],
+  ownerAbilities: ['Manage restaurant profile', 'Manage restaurant tables', 'Invite Chefs and Waiters to join the app'],
+};
